43aaa22f6754ecf2e4a3063a6441ec29e7c19441
nazmulkhanliton <liton079@gmail.com>
2025-02-02 01:33:18 +0600

    DB data creat

---
+++ script.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  //   const user = await prisma.user.create({ data: { name: "Khan" } });
+  //   console.log(user);
+  const user = await prisma.user.findMany();
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
